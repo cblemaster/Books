fdee6aa5f055509005fc14d8932df860d32e79ca
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-22 11:42:47 -0500

    moved db conn string out of db context and into config in API project

--- Books.API/Context/BooksContext.cs
+++ Books.API/Context/BooksContext.cs
@@ -21,8 +21,9 @@ public partial class BooksContext : DbContext
     public virtual DbSet<Genre> Genres { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=Books;Trusted_Connection=true;Trust Server Certificate=true");
+    {
+
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
