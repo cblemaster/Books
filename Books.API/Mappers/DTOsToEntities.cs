9cb961b12447351b4625c60cccff1ed3e8e1d55c
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-22 14:08:15 -0500

    added entity to dto and dto to entity mappers in API project

---
+++ Books.API/Mappers/DTOsToEntities.cs
@@ -0,0 +1,121 @@
+﻿using Books.API.DTOs;
+using Books.Core.Entities;
+
+namespace Books.API.Mappers
+{
+    public static class DTOsToEntities
+    {
+        public static Author MapCreateAuthorDTOToAuthorEntity(CreateAuthor createAuthor)
+        {
+            if (createAuthor is null)
+            {
+                return Author.NotFound;
+            }
+            return new Author()
+            {
+                AuthorName = createAuthor.AuthorName,
+            };
+        }
+
+        public static Book MapCreateBookDTOToBookEntity(CreateBook createBook)
+        {
+            if (createBook is null)
+            {
+                return Book.NotFound;
+            }
+            return new Book()
+            {
+                Title = createBook.Title,
+                AuthorId = createBook.AuthorId,
+                PageCount = createBook.PageCount,
+                PublicationYear = createBook.PublicationYear,
+                Genres = MapCollectionOfReadGenreDTOToCollectionOfGenreEntities(createBook.Genres).ToList(),
+            };
+        }
+
+        public static Genre MapCreateGenreDTOToGenreEntity(CreateGenre createGenre)
+        {
+            if (createGenre is null)
+            {
+                return Genre.NotFound;
+            }
+            return new Genre()
+            {
+                GenreName = createGenre.GenreName,
+            };
+        }
+
+        public static Author MapUpdateAuthorDTOToAuthorEntity(UpdateAuthor updateAuthor)
+        {
+            if (updateAuthor is null)
+            {
+                return Author.NotFound;
+            }
+            return new Author()
+            {
+                AuthorId = updateAuthor.AuthorId,
+                AuthorName = updateAuthor.AuthorName,
+            };
+        }
+
+        public static Book MapUpdateBookDTOToBookEntity(UpdateBook updateBook)
+        {
+            if (updateBook is null)
+            {
+                return Book.NotFound;
+            }
+            return new Book()
+            {
+                BookId = updateBook.BookId,
+                Title = updateBook.Title,
+                AuthorId = updateBook.AuthorId,
+                PageCount = updateBook.PageCount,
+                PublicationYear = updateBook.PublicationYear,
+                Genres = MapCollectionOfReadGenreDTOToCollectionOfGenreEntities(updateBook.Genres).ToList(),
+            };
+        }
+
+        public static Genre MapUpdateGenreDTOToGenreEntity(UpdateGenre updateGenre)
+        {
+            if (updateGenre is null)
+            {
+                return Genre.NotFound;
+            }
+            return new Genre()
+            {
+                GenreId = updateGenre.GenreId,
+                GenreName = updateGenre.GenreName,
+            };
+        }
+
+        public static Genre MapReadGenreDTOToGenreEntity(ReadGenre genre)
+        {
+            if (genre is null)
+            {
+                return Genre.NotFound;
+            }
+            return new Genre()
+            {
+                GenreId = genre.GenreId,
+                GenreName = genre.GenreName,
+            };
+        }
+
+        public static IEnumerable<Genre> MapCollectionOfReadGenreDTOToCollectionOfGenreEntities(IEnumerable<ReadGenre> genres)
+        {
+            if (genres is null || !genres.Any())
+            {
+                return Genre.CollectionNotFound;
+            }
+            List<Genre> entityCollection = [];
+
+            foreach (ReadGenre genre in genres)
+            {
+                Genre entityGenre = MapReadGenreDTOToGenreEntity(genre);
+                entityCollection.Add(entityGenre);
+            }
+
+            return entityCollection.AsEnumerable<Genre>();
+        }
+    }
+}
---
+++ Books.API/Mappers/EntitiesToDTOs.cs
@@ -0,0 +1,106 @@
+﻿using Books.API.DTOs;
+using Books.Core.Entities;
+
+namespace Books.API.Mappers
+{
+    public static class EntitiesToDTOs
+    {
+        public static ReadAuthor MapAuthorEntityToReadAuthorDTO(Author author)
+        {
+            if (author is null)
+            {
+                return ReadAuthor.NotFound;
+            }
+            return new ReadAuthor()
+            {
+                AuthorId = author.AuthorId,
+                AuthorName = author.AuthorName,
+            };
+        }
+        
+        public static ReadBook MapBookEntityToReadBookDTO(Book book)
+        {
+            if (book is null)
+            {
+                return ReadBook.NotFound;
+            }
+            return new ReadBook()
+            {
+                BookId = book.BookId,
+                Title = book.Title,
+                AuthorId = book.AuthorId,
+                Author = MapAuthorEntityToReadAuthorDTO(book.Author),
+                PageCount = book.PageCount,
+                PublicationYear = book.PublicationYear,
+                Genres = MapCollectionOfGenreEntitiesToCollectionOfReadGenreDTO(book.Genres),
+            };
+        }
+
+        public static ReadGenre MapGenreEntityToReadGenreDTO(Genre genre)
+        {
+            if (genre is null)
+            {
+                return ReadGenre.NotFound;
+            }
+            return new ReadGenre()
+            {
+                GenreId = genre.GenreId,
+                GenreName = genre.GenreName,
+            };
+        }
+
+        public static IEnumerable<ReadAuthor> MapCollectionOfAuthorEntitiesToCollectionOfReadAuthorDTO(IEnumerable<Author> authors)
+        {
+            if (authors is null || !authors.Any())
+            {
+                return ReadAuthor.CollectionNotFound;
+            }
+
+            List<ReadAuthor> dtoCollection = [];
+
+            foreach (Author author in authors)
+            {
+                ReadAuthor dtoAuthor = MapAuthorEntityToReadAuthorDTO(author);
+                dtoCollection.Add(dtoAuthor);
+            }
+
+            return dtoCollection.AsEnumerable<ReadAuthor>();
+        }
+
+        public static IEnumerable<ReadBook> MapCollectionOfBookEntitiesToCollectionOfReadBookDTO(IEnumerable<Book> books)
+        {
+            if (books is null || !books.Any())
+            {
+                return ReadBook.CollectionNotFound;
+            }
+
+            List<ReadBook> dtoCollection = [];
+
+            foreach (Book book in books)
+            {
+                ReadBook dtoBook = MapBookEntityToReadBookDTO(book);
+                dtoCollection.Add(dtoBook);
+            }
+
+            return dtoCollection.AsEnumerable<ReadBook>();
+        }
+
+        public static IEnumerable<ReadGenre> MapCollectionOfGenreEntitiesToCollectionOfReadGenreDTO(IEnumerable<Genre> genres)
+        {
+            if (genres is null || !genres.Any())
+            {
+                return ReadGenre.CollectionNotFound;
+            }
+
+            List<ReadGenre> dtoCollection = [];
+
+            foreach (Genre genre in genres)
+            {
+                ReadGenre dtoGenre = MapGenreEntityToReadGenreDTO(genre);
+                dtoCollection.Add(dtoGenre);
+            }
+
+            return dtoCollection.AsEnumerable<ReadGenre>();
+        }
+    }
+}
