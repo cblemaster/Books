f143ccfdd01746fc41200074b72373590def4fb3
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-23 16:33:12 -0500

    forgot the interface with the last commit...

---
+++ Books.Core/Services/HttpService/IHttpService.cs
@@ -0,0 +1,23 @@
+﻿using Books.Core.Services.DTOs;
+
+namespace Books.Core.Services.HttpService
+{
+    public interface IHttpService
+    {
+        Task<ReadAuthor> GetAuthorByIdAsync(int authorId);
+        Task<IEnumerable<ReadAuthor>> GetAuthorsAsync();
+        Task<ReadBook> GetBookByIdAsync(int bookId);
+        Task<IEnumerable<ReadBook>> GetBooksAsync();
+        Task<ReadGenre> GetGenreByIdAsync(int genreId);
+        Task<IEnumerable<ReadGenre>> GetGenresAsync();
+        Task<ReadAuthor> CreateAuthorAsync(CreateAuthor createAuthor);
+        Task<ReadBook> CreateBookAsync(CreateBook createBook);
+        Task<ReadGenre> CreateGenreAsync(CreateGenre createGenre);
+        void UpdateAuthorAsync(int authorId, UpdateAuthor updateAuthor);
+        void UpdateBookAsync(int bookId, UpdateBook updateBook);
+        void UpdateGenreAsync(int genreId, UpdateGenre updateGenre);
+        void DeleteAuthorAsync(int authorId);
+        void DeleteBookAsync(int bookId);
+        void DeleteGenreAsync(int genreId);
+    }
+}
